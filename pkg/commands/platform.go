db3188e3513a66c321ff44c48abd583dc1bba775
Oliver Hughes <ohughes@pivotal.io>
2019-08-28 12:04:22 +0100

    Set up CI with Azure Pipelinesconfigure golangci-lint to be run in Makefile and within CIfix existing linting issues

--- pkg/commands/platform.go
+++ pkg/commands/platform.go
@@ -1,19 +1,24 @@
 package commands
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+// PlatformCommand has no direct actions. It is a container for subcommands such as `platform list`
 func PlatformCommand() *cobra.Command {
 	platformCmd := &cobra.Command{
 		Use:   "platform",
 		Short: "Platform commands",
 		Long:  `Commands related to platform operations`,
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-			os.Exit(1)
+			err := cmd.Help()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %v\n", err)
+				os.Exit(1)
+			}
 		},
 	}
 	platformCmd.AddCommand(PlatformListCommand())
